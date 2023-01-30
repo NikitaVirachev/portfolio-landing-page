3bdfb696a79f25bbca7f5a3463e12fe42b2894f7
NikitaVirachev <guardsolder@gmail.com>
2023-01-30 14:17:07 +0300

    feat: implement mobile nav functionality

---
+++ js/script.js
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavOpenEl = document.querySelector(".btn-mobile-nav-open");
+const btnNavCloseEl = document.querySelector(".btn-mobile-nav-close");
+const headerEl = document.querySelector(".header");
+
+btnNavOpenEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+btnNavCloseEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
